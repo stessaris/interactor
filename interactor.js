17f01bc56c616d09fa566930077cdc0ab942f2bb
Sergio Tessaris <39191293+stessaris@users.noreply.github.com>
2021-12-03 12:29:01 +0100

    Version 1.5.3+nxtg

--- interactor.js
+++ interactor.js
@@ -26,7 +26,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-const _version_ = '1.5.2+nxtg';
+const _version_ = '1.5.3+nxtg';
 
 var Interactor = function (config) {
     // Call Initialization on Interactor Call
